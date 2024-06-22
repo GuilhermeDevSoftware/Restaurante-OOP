587187561706fc8de0dee8f0e947932e19f467ff
Guilherme Costa <guizeracostasilva@outlook.com>
2024-06-22 11:21:16 -0300

    Avaliando restaurantes

--- app.py
+++ app.py
@@ -2,8 +2,26 @@ from modelos.restaurante import Restaurante
 
 restaurante_padoca = Restaurante('Padoca', 'Padaria')
 restaurante_mexicano = Restaurante('Mexican food', 'Mexicano')
-restaurante_japones = Restaurante('Japa food', 'Japonês')
+restaurante_pizza = Restaurante('Pizzaria Italy', 'Pizzaria')
+restaurante_hamburguer = Restaurante('Hamburgueria El', 'Hamburgueria')
 
+restaurante_padoca.receber_avaliacao('Guilherme', 10)
+restaurante_padoca.receber_avaliacao('Lais', 8)
+restaurante_padoca.receber_avaliacao('Jorge', 7.5)
+
+restaurante_mexicano.receber_avaliacao('Silvia', 9)
+restaurante_mexicano.receber_avaliacao('Gabriel', 2)
+restaurante_mexicano.receber_avaliacao('Jeremias', 5)
+
+restaurante_pizza.receber_avaliacao('Rodrigo', 10)
+restaurante_pizza.receber_avaliacao('Camila', 6)
+restaurante_pizza.receber_avaliacao('William', 8)
+
+restaurante_hamburguer.receber_avaliacao('Jaqueline', 4)
+restaurante_hamburguer.receber_avaliacao('Larissa', 7)
+restaurante_hamburguer.receber_avaliacao('Emmily', 10)
+
+restaurante_hamburguer.alternar_estado()
 restaurante_mexicano.alternar_estado()
 
 def main():
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,3 +1,5 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
     restaurantes = []
 
@@ -5,21 +7,31 @@ class Restaurante:
         self._nome = nome.title() #self indica do construtor
         self._categoria = categoria.upper()
         self._ativo = False #o _ indica que é um conteudo privado, usuario não modifica
+        self._avalicao = [] #recebe uma lista
         Restaurante.restaurantes.append(self)
-
-    def __str__(self): #Para mostrar como string, legivel para nós
-        return f'{self._nome} || {self._categoria}'   
-    
+        
     @classmethod #indica que é um metodo da class, um metodo criado por nós, ou seja, uma função
     def listar_restaurantes(cls):
-        print(f'{"Nome do restaurante".ljust(20)} | {"Categoria".ljust(20)} | {"Status"}')
+        print(f'{"Nome do restaurante".ljust(20)} | {"Categoria".ljust(20)} | {"Avaliações".ljust(20)} | {"Status"}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(20)} | {restaurante._categoria.ljust(20)} | {restaurante.ativo}')
-
+            print(f'{restaurante._nome.ljust(20)} | {restaurante._categoria.ljust(20)} | {str(restaurante.media_avaliacoes).ljust(20)} | {restaurante.ativo}')
+            
     @property
     def ativo(self):
         return '⌧' if self._ativo else '☐'
     
     def alternar_estado(self):
         self._ativo = not self._ativo
+                        #self significa o objeto
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avalicao.append(avaliacao)
 
+    @property
+    def media_avaliacoes(self):
+        if not self._avalicao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avalicao) #aqui soma a quatidade de notas dentro do obj avaliação
+        quantidade_de_notas = len(self._avalicao) #aqui ve o tanto de avaliação que o restaurante recebeu
+        media = round(soma_das_notas / quantidade_de_notas, 1) #o round arredonda o calculo, nesse caso 1 case decimal
+        return media
